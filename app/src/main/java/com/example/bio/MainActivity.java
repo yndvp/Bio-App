1f199b5c1dc005f20ea7996f16e38e9ea608f345
yndvp <58580623+yndvp@users.noreply.github.com>
2022-01-29 19:05:12 -0500

    use linear layout / add styles.xml, dimens.xml / use data binding

--- app/src/main/java/com/example/bio/MainActivity.java
+++ app/src/main/java/com/example/bio/MainActivity.java
@@ -1,14 +1,59 @@
 package com.example.bio;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.bio.data.Bio;
+import com.example.bio.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    // View binding
+    private ActivityMainBinding binding;
+    // Data binding
+    private Bio bio = new Bio();
+
+//    private EditText enterHobbies;
+//    private TextView hobbiesText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        // View binding
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding.doneButton.setOnClickListener(view -> addHobbies(view));
+        // Data binding
+        bio.setName("Yunna Jang");
+        binding.setBio(bio);
+
+//        setContentView(R.layout.activity_main);
+//
+//        enterHobbies = findViewById(R.id.enterHobbies);
+//        hobbiesText = findViewById(R.id.hobbiesText);
+    }
+
+    public void addHobbies(View view) {
+        // Data binding
+        bio.setHobbies(String.format("Hobbies: %s", binding.enterHobbies.getText().toString().trim()));
+
+        // View binding
+//        binding.hobbiesText.setText(String.format("Hobbies: %s", binding.enterHobbies.getText().toString().trim()));
+        binding.invalidateAll();
+        binding.hobbiesText.setVisibility(View.VISIBLE);
+//        hobbiesText.setText(String.format("Hobbies: %s", enterHobbies.getText().toString().trim()));
+//        hobbiesText.setVisibility(View.VISIBLE);
+
+        // Hide keyboard if we enter done button
+        InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        assert inputMethodManager != null;
+        inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
     }
 }
---
+++ app/src/main/java/com/example/bio/data/Bio.java
@@ -0,0 +1,31 @@
+package com.example.bio.data;
+
+public class Bio {
+    private String name;
+    private String hobbies;
+
+    public Bio(){
+
+    }
+
+    public Bio(String name, String hobbies) {
+        this.name = name;
+        this.hobbies = hobbies;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getHobbies() {
+        return hobbies;
+    }
+
+    public void setHobbies(String hobbies) {
+        this.hobbies = hobbies;
+    }
+}
